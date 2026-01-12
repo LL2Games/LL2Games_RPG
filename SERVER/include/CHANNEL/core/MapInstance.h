178d99091fdef93fd46f4c19ed875baa9dac1bdc
dyddlswogh <dyddlswogh>
2026-01-12 12:22:27 +0900

    feat: ChannelServer 재커밋  - MapInstance Monster관련 메스드 및 변수 추가  - MapManager JSON 파일을 이용해서 맵 정보 로드 및 몬스터 정보 로드 추가  - CommonEnum.h 몬스터 정보 저장 구조체 및 사용 안하는 구조체 삭제  - Json 기능 사용을 위해 json헤더 파일 추가

--- SERVER/include/CHANNEL/core/MapInstance.h
+++ SERVER/include/CHANNEL/core/MapInstance.h
@@ -2,6 +2,7 @@
 #include "CHANNEL/core/common.h"
 #include "CHANNEL/core/PlayerManager.h"
 #include "COMMON/CommonEnum.h"
+#include "CHANNEL/core/Monster.h"
 
 
 class MapInstance 
@@ -11,6 +12,7 @@ public:
     ~MapInstance();
     
     int Init(const MapInitData& data);
+    int InitSpawnMonster();
     int SetMonster(MonsterType monsterType);
     int Update(float deltaTime);
     int SpawnMonster();
@@ -20,7 +22,9 @@ private:
     //uint16_t  m_map_id;
     bool  m_has_player;
     MapRegion m_mapID;
-    std::vector<SpawnStatus> m_spawnList;
+    std::vector<MonsterSpawnData> m_monsterSpawnList;
+    std::vector<MonsterSpawnData>::iterator m_monsterSpawnListIter;
 
-    
+    std::vector<Monster> m_monsterList;
+    std::vector<Monster>::iterator m_monsterListIter;
 };
---
+++ SERVER/include/CHANNEL/core/Monster.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "CHANNEL/core/common.h"
+#include "COMMON/CommonEnum.h"
+
+class Monster
+{
+public:
+    Monster(){};
+    ~Monster(){};
+
+    void SetPos(Vec2 Pos){this->m_Pos.xPos = Pos.xPos; this->m_Pos.yPos = Pos.yPos;}
+private:
+    MonsterType m_type;
+    Vec2 m_Pos;
+    bool m_isAlive;
+
+    int m_hp;
+    int m_maxhp;
+
+    //몬스터 상태 값 나중에 추가해야함
+    //MonsterState state;
+    float m_attackDamage;
+    int m_level;
+
+};
